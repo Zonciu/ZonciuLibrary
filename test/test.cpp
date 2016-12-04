dbfb2fbea9af355f5442417552d5ffbf826eb69f
Zonciu <zonciu@zonciu.com>
2016-12-05 02:19:44 +0800

    Fix md5 bug.

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,6 @@
 #include "zonciu/util.hpp"
 #include "zonciu/lock.hpp"
 #include "zonciu/random.hpp"
-#include "zonciu/security.hpp"
 #include "zonciu/singleton.hpp"
 #include "zonciu/semaphor.hpp"
 #include "zonciu/format.hpp"
@@ -11,6 +10,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <gtest/gtest.h>
+#include <windows.h>
 using namespace std;
 
 TEST(Random_Num, random_num_test)
@@ -18,14 +18,13 @@ TEST(Random_Num, random_num_test)
     EXPECT_LE(10, zonciu::Random::Num(10, 9999));
     EXPECT_GE(9999, zonciu::Random::Num(10, 9999));
 }
-int test(int argc,char*argv[])
+int test(int argc, char*argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
 int main(int argc, char *argv[])
 {
-    zonciu::Json js;
-    cout<<as.dump();
-    
+    zonciu::MinHeapTimer t;
 }
